3cf689add36cccda4f9eff4e730f7a43720a1440
biela000 <jak.biela000@gmail.com>
2022-11-11 18:02:00 +0100

    Repair cors, prepareBody middleware

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 const boardRouter = require('./routes/boardRouter');
 const errorHandler = require('./utils/errorHandler');
 const AppError = require('./utils/appError');
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PATCH');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
--- controllers/boardController.js
+++ controllers/boardController.js
@@ -11,6 +11,7 @@ exports.prepareBody = (req, res, next) => {
       )
     );
   }
+  next();
 };
 
 exports.getAllBoards = catchAsync(async (req, res, next) => {
